bf4191397f89e173b31be8082379d8548d73a737
Gungcakra <agungcakra888@gmail.com>
2025-01-12 14:15:28 +0800

    Add bottom navigation bar to user interface and include configuration file

---
+++ user/bottomBar.php
@@ -0,0 +1,36 @@
+
+    <nav class="navbar navbar-light navbar-expand rounded-pill mb-3 ms-3 me-3 fixed-bottom d-md-none d-lg-none d-xl-none shadow"
+        style="background: #007bff;">
+        <ul class="nav nav-justified w-100" id="myTab" role="tablist">
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="search-tab" data-bs-toggle="tab" data-bs-target="#search" type="button"
+                    role="tab" aria-controls="search" aria-selected="false">
+                    <span><i class="fas fa-search text-white"></i></span>
+                </button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="notifications-tab" data-bs-toggle="tab" data-bs-target="#notifications" type="button"
+                    role="tab" aria-controls="notifications" aria-selected="false">
+                    <span><i class="fas fa-bell text-white"></i></span>
+                </button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button"
+                    role="tab" aria-controls="home" aria-selected="true">
+                    <span><i class="fas fa-home text-white"></i></span>
+                </button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="messages-tab" data-bs-toggle="tab" data-bs-target="#messages" type="button"
+                    role="tab" aria-controls="messages" aria-selected="false">
+                    <span><i class="fas fa-envelope text-white"></i></span>
+                </button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button"
+                    role="tab" aria-controls="profile" aria-selected="false">
+                    <span><i class="fas fa-user text-white"></i></span>
+                </button>
+            </li>
+        </ul>
+    </nav>
--- user/index.php
+++ user/index.php
@@ -1,3 +1,6 @@
+<?php
+require_once '../library/config.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -31,6 +34,8 @@
                         <div class="text-danger fs-3 fw-bold mt-2">Absen Gagal!</div>
                 </div>
         </div>
+        <?php require_once "{$constant('BASE_URL_PHP')}/user/bottomBar.php"; ?>
+
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
         <script>
